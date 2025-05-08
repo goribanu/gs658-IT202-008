076e525f73e01824ef64c4b3eddabe186c725269
goribanu <gs658@njit.edu>
2025-05-07 23:51:10 -0400

    updating api stuff

--- lib/load_api_keys.php
+++ lib/load_api_keys.php
@@ -1,26 +1,18 @@
 <?php
-// string array containing env keys to lookup (this allows usage of multiple APIs)
 $env_keys = ["OMDB_API_KEY"];
 $ini = @parse_ini_file(".env");
 
 $API_KEYS = [
-    "OMDB_API_KEY" => "5a599b6d"
+    "OMDB_API_KEY" => "5a599b6d" // default fallback
 ];
 
 foreach ($env_keys as $key) {
-    if ($ini && isset($ini[$key])) {
-        //load local .env file
-        $API_KEY = $ini[$key];
-        $API_KEYS[$key] = $API_KEY;
+    $env_value = $ini[$key] ?? getenv($key);
+    if (!empty($env_value)) {
+        $API_KEYS[$key] = $env_value;
     } else {
-        //load from heroku env variables
-        $API_KEY = getenv($key);
-        $API_KEYS[$key] = $API_KEY;
+        error_log("Failed to load API key for env key $key");
     }
-    if (!isset($API_KEYS[$key]) || !$API_KEYS[$key]) {
-        error_log("Faild to load api key for env key $key");
-    }
-    unset($API_KEY);
 }
 
 error_log("Loaded API Keys: " . var_export($API_KEYS, true));
